b55bcb674998b03b94f26917a76685933afdae15
carloscoss <crobertohenriquez@gmail.com>
2026-04-23 13:41:12 -0500

    V1

--- src/main/java/com/pluralsight/StoreApp.java
+++ src/main/java/com/pluralsight/StoreApp.java
@@ -1,6 +1,7 @@
 import com.pluralsight.Product;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class StoreApp {
     public static void main(String[] args) {
         ArrayList<Product> inventory = getInventory();
@@ -13,8 +14,24 @@ public class StoreApp {
     }
     public static ArrayList<Product> getInventory() {
         ArrayList<Product> inventory = new ArrayList<Product>();
+        // inventory of music instruments
+        inventory.add(new Product(1, "Electric Guitar", 1699.00f));
+        inventory.add(new Product(2, "Bass Guitar", 1350.00f));
+        inventory.add(new Product(3, "Keyboard", 1400.00f));
+        inventory.add(new Product(4, "Microphone", 1800.00f));
+        inventory.add(new Product(5, "Drums", 4000.00f));
+
+
+
+
+
+
+
+
 // this method loads product objects into inventory
 // and its details are not shown
         return inventory;
     }
+
+
 }
